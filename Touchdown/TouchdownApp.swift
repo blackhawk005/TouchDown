bafd05893e9addf686900b78acd37b703d52303c
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2021-04-11 01:41:15 +0530

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Shinit Shetty on 11/04/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
